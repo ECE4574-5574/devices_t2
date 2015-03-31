a95b63a7bc7d3457baf3f0b78464af9d3520ecee
sscho91 <sscho91@vt.edu>
2015-03-30 20:53:15 -0400

    Update DeviceMedium.cs

--- DeviceMedium.cs
+++ DeviceMedium.cs
@@ -1,83 +1,111 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 //using MySql.Data.MySqlClient; 
 namespace ASSIGN5TEAM2
 {
-    class DeviceMedium : device
+    class DeviceMedium : DeviceAPI
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
-            ArrayList deviceList = new ArrayList();
-
-            //Read from Server
-            /*
-            public List<string>[] getDeviceList()
-            {
-            string query = "select * from cart c inner join product p ON p.asin = c.asin";
-            List<string>[] list = new List<string>[1];
-		    list[0] = new List<string>();
-
-		    if (OpenConnection()) //Open MYSQL connection
-            {
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                try
-                {
-                    MySqlDataReader reader = cmd.ExecuteReader();
-                    while(reader.Read())
-                    {
-                        list[0].Add(reader["devType"].ToString());
-                        // Add function:  add(id, devType, status)
-                    }
-                }
-                catch (MySqlException e)
-                {
-                    CloseConnection(); //Close MYSQL Connection
-                    return null;
-                }
-            }
-            CloseConnection(); //Close MYSQL connection
-            return list;
-            } */
+     
+            //string json = Console.In.ReadToEnd();
+            //WebClient c = new WebClient();
+            //var json = c.DownloadString("simharn_url");
+            //Houses house = JsonConvert.DeserializeObject<Houses>(json);
+            
+           // devices deviceJ = JsonConvert.DeserializeObject<devices>(json);
+            //bool run = true; while loop testing
+            string answer, output;
 
 
-            // testing garage door object to get ID, Device Name, Room ID, and Status
-            GarageDoor boo = new GarageDoor();
-
-            boo.addDevice(2, "Garage1", false);
-            Console.WriteLine(boo.getid());
-            Console.WriteLine(boo.getDeviceType());
-            Console.WriteLine(boo.getStatus());
-
-            deviceList.Add(boo);
-
-            sprinkler boo2 = new sprinkler();
-
-            boo2.addDevice(4, "Sprinkler", false);
-            Console.WriteLine(boo2.getid());
-            Console.WriteLine(boo2.getDeviceType());
-            Console.WriteLine(boo2.getStatus());
-            Console.WriteLine("change status of sprinkler...");
-            boo2.turnOn();
-            Console.WriteLine(boo2.getStatus());
-            deviceList.Add(boo2);
-
-            Console.WriteLine("Testing deviceList");
-            // Display the values.
-            //
-            foreach (device i in deviceList)
-            {
-                Console.WriteLine("ID: {0} \n" +
-                                  "Device: '{1}' \n" +
-                                  "Status: {2} \n",
-                                  i.getid(), i.getDeviceType(), i.getStatus());
-            }
+            string devid, devName, type //garage, light, thermostat, alarmsystem, motionsensor, refrigerator, etc 
+                , roomid, state;
+            string brightness, speed, temp;
+            int conid, conroom , conbri = 0, conspeed = 0, contemp = 0;
+            bool boolVal;
 
             
+            //Console.WriteLine("Enter devid:");
+            
+            
+            /*
+            type = dev_types;
+            if (type == Lightss)
+            {
 
+                //devid = //deserialize(;
+                conid = Convert.ToInt32(devid);
+                devName = house.light_name;
+                //roomid = house.light_room;
+                //conroom = Convert.ToInt32(roomid);
+                state = house.light_startstate;
+                brightness = house.light_brightness;
+                conbri = Convert.ToInt32(brightness);
+            }
+            else if (type == Ceilingfans)
+            {
+                //devid, 
+                devName = house.cf_name;
+                //roomid = house.dev_id;
+                //conroom = Convert.ToInt32(roomid);
+                state = house.cf_startstate;
+                speed = cf_speed;
+                conspeed = Convert.ToInt32(speed);
+            }
+            else if (type == "thermostat")
+            {
+                devName = house.t_name;
+                //roomid = house.t_room;
+                //conroom = Convert.ToInt32(roomid);
+                state = house.t_startstate;
+                temp = house.t_temp;
+                contemp = Convert.ToInt32(temp);
+            }
+            else
+            {
+
+            }
+            */
+            switch (type)
+            {
+                
+                case "light":
+                    Light boo2 = new Light();
+                    output = JsonConvert.SerializeObject(boo2);
+                    boo2.addDevice(conid, devName, type, conroom, boolVal, conbri);
+                    Console.WriteLine(boo2.getid());
+                    Console.WriteLine(boo2.getDeviceType());
+                    Console.WriteLine(boo2.getStatus());
+                    break;
+                case "ceilingfan":
+                    Ceilingfan boo3 = new Ceilingfan();
+                    output = JsonConvert.SerializeObject(boo3);
+                    boo3.addDevice(conid, devName, type, conroom, boolVal);
+                    Console.WriteLine(boo3.getid());
+                    Console.WriteLine(boo3.getDeviceType());
+                    Console.WriteLine(boo3.getStatus());
+                    break;
+                case "garage":
+                    Garagedoor boo = new Garagedoor();
+                    output = JsonConvert.SerializeObject(boo);
+                    boo.addDevice(conid, devName, type, conroom, boolVal);
+                    Console.WriteLine(boo.getid());
+                    Console.WriteLine(boo.getDeviceType());
+                    Console.WriteLine(boo.getStatus());
+                    break;     
+
+                default:
+                    break;
+            }
+           
         }
+
+
     }
+
 }
