54ff5aba9ed78175f468e5381ed81c978bdb5b02
Jason Ziglar <jpz@vt.edu>
2015-04-09 10:42:14 -0400

    Adding stubs for DeviceIO.

---
+++ api/HouseInput.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * Reading device state from the house app.
+ */
+using System;
+
+namespace api
+{
+/**
+ * Class which reads state from the house app.
+ */
+public class HouseInput : IDeviceInput
+{
+	public HouseInput()
+	{
+	}
+
+	public bool read(Device dev)
+	{
+		return true;
+	}
+}
+}
+
---
+++ api/ServerOutput.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Input for writing device state to the Server API
+ */
+using System;
+
+namespace api
+{
+public class ServerOutput : IDeviceOutput
+{
+	public ServerOutput()
+	{
+	}
+
+	public bool write(Device dev)
+	{
+		//TODO: Implement this
+		return true;
+	}
+}
+}
+
