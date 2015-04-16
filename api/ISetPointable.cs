3144adb92fa28afa0b91dfdca0968fd68210b59b
Jason Ziglar <jpz@vt.edu>
2015-04-15 23:22:03 -0400

    Making interfaces public for querying.

--- api/ISetPointable.cs
+++ api/ISetPointable.cs
@@ -9,7 +9,7 @@ namespace api
 /**
  * Interface defining how to set a set point for the behavior of a given Device
  */
-interface ISetPointable<Type> where Type : ControlTypes
+public interface ISetPointable<Type> where Type : ControlTypes
 {
 	/**
 	 * Target set point of this device. For example, the set point of a thermostat
