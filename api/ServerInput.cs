78d1420e50067519ca11e2b2cdd9c5970c26a959
Dong <raitodong@gmail.com>
2015-04-21 23:31:45 -0400

    Update ServerInput.cs

--- api/ServerInput.cs
+++ api/ServerInput.cs
@@ -16,6 +16,22 @@ public class ServerInput : IDeviceInput
 	 */
 	public bool read(Device dev)
 	{
+		//DeviceRepository rep = new DeviceRepository();
+	        string houseid = dev.id.houseid.ToString();
+	        string roomid = dev.id.roomid.ToString();
+	        string deviceid = dev.id.deviceid.ToString();
+	        //string json = rep.GetDevice(houseid, roomid, deviceid).ToString();
+	        string url = "serverapi1.azurewebsites.net";
+	        HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+	        request.Method = "GET";
+	        WebHeaderCollection headers = (request as HttpWebRequest).Headers;
+	        headers.Add("api/house/device/" + houseid + "/" + roomid + "/" + deviceid);
+	        HttpWebResponse response;
+	        response = request.GetResponse() as HttpWebResponse;
+	        Stream stream = response.GetResponseStream();
+	        StreamReader readStream = new StreamReader(stream, Encoding.UTF8);
+	        string json = readStream.ReadToEnd();
+	        return Interfaces.UpdateDevice(dev, json);
 		//TODO: Implement this function
 		return true;
 	}
