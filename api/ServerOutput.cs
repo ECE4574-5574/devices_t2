dbb6aefcc7934637803c899530bf64d76fc3f6ac
Dong <raitodong@gmail.com>
2015-04-22 00:01:59 -0400

    Update ServerOutput.cs

--- api/ServerOutput.cs
+++ api/ServerOutput.cs
@@ -13,6 +13,14 @@ public class ServerOutput : IDeviceOutput
 
 	public bool write(Device dev)
 	{
+		string houseid = dev.id.houseid.ToString();
+	        string roomid = dev.id.roomid.ToString();
+	        string deviceid = dev.id.deviceid.ToString();
+	        string url = "serverapi1d.azurewebsites.net";
+	        HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+	        request.Method = "POST";
+	        WebHeaderCollection headers = (request as HttpWebRequest).Headers;
+	        headers.Add("api/house/device/" + houseid + "/" + roomid + "/" + deviceid);
 		return true;
 	}
 }
