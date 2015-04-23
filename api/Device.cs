78014bfeab83f37175b127ecd296080ac93903fc
willrd9 <wr.doan@hotmail.com>
2015-04-23 10:21:01 -0400

    Added IODevices to setters/getters and update

--- api/Device.cs
+++ api/Device.cs
@@ -8,98 +8,114 @@ using Newtonsoft.Json;
 
 namespace api
 {
-/**
- * Base class representing the common parameters for any given device. All Devices inherit from this
- */
-public abstract class Device
-{
-	public Device(IDeviceInput inp, IDeviceOutput outp, TimeFrame frame)
-	{
-		_in = inp;
-		_out = outp;
-		_frame = frame;
-		_last_time = DateTime.MinValue; //Set to minimum possible time
-		_id = new FullID();
-		_name = "";
-	}
-
-	public FullID ID
-	{
-		get
-		{
-			return _id;
-		}
-		set
-		{
-			_id = value;
-		}
-	}
-
-	public DateTime LastUpdate
-	{
-		get
-		{
-			return _last_time;
-		}
-		set
-		{
-			_last_time = value;
-		}
-	}
-
 	/**
-	 * User friendly name for this device.
+	 * Base class representing the common parameters for any given device. All Devices inherit from this
 	 */
-	public string Name
+	public abstract class Device
 	{
-		get
+		public Device(IDeviceInput inp, IDeviceOutput outp, TimeFrame frame)
 		{
-			return _name;
+			_in = inp;
+			_out = outp;
+			_frame = frame;
+			_last_time = DateTime.MinValue; //Set to minimum possible time
+			_id = new FullID();
+			_name = "";
 		}
-		set
+
+		public FullID ID
 		{
-			_name = value;
+			get
+			{
+				if (_in.read(this))
+					return _id;
+				else
+					return null;
+			}
+			set
+			{
+				_id = value;
+				_out.write(this);
+			}
 		}
 
-	}
-
-	public string Class
-	{
-		get;
-		set;
-	}
-
-	[JsonIgnore]
-	public TimeFrame Frame
-	{
-		get
+		public DateTime LastUpdate
 		{
-			return _frame;
+			get
+			{
+				if (_in.read(this))
+					return _last_time;
+				else
+					return System.DateTime.MinValue;
+			}
+			set
+			{
+				_last_time = value;
+				_out.write(this);
+			}
 		}
-		set
+
+		/**
+		 * User friendly name for this device.
+		 */
+		public string Name
 		{
-			_frame = value;
+			get
+			{
+				if (_in.read(this))
+					return _name;
+				else
+					return null;
+			}
+			set
+			{
+				_name = value;
+				_out.write(this);
+			}
+
 		}
-	}
 
-	/**
-	 * Grabs the latest available information about this device, and updates
-	 * internal state to match it. This should also update LastUpdated as a post condition.
-	 * \param[out] Flag indicating success
-	 */
-	public bool update()
-	{
-		//TODO: This function should grab the latest state of the device
-		//using the IDeviceInput, without worrying about any particular parameter
-		return true;
-	}
+		public string Class
+		{
+			get;
+			set;
+		}
 
-	protected IDeviceInput _in;
-	protected IDeviceOutput _out;
-	protected TimeFrame _frame;
-	protected DateTime _last_time;
-	protected string _name;
-	protected FullID _id;
-}
+		[JsonIgnore]
+		public TimeFrame Frame
+		{
+			get
+			{
+				if (_in.read(this))
+					return _frame;
+				else
+					return null;
+			}
+			set
+			{
+				_frame = value;
+				_out.write(this);
+			}
+		}
+
+		/**
+		 * Grabs the latest available information about this device, and updates
+		 * internal state to match it. This should also update LastUpdated as a post condition.
+		 * \param[out] Flag indicating success
+		 */
+		public bool update()
+		{
+			//TODO: This function should grab the latest state of the device
+			//using the IDeviceInput, without worrying about any particular parameter
+			return _in.read(this);
+		}
+
+		protected IDeviceInput _in;
+		protected IDeviceOutput _out;
+		protected TimeFrame _frame;
+		protected DateTime _last_time;
+		protected string _name;
+		protected FullID _id;
+	}
 
 }
