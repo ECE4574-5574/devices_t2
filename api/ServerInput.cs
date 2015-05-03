d7a70703581dd35d1f03f57319f859f5045213e5
Jason Ziglar <jpz@vt.edu>
2015-05-03 08:57:41 -0400

    Partial ServerInput fix.
    Missing wait() call, so response was always invalid.
    Leading / was incorrect in resource_address, causing the GET to never
    succeed.
    Several error checks were missing.
    
    This still causes stack overflows when executing.

--- api/ServerInput.cs
+++ api/ServerInput.cs
@@ -16,18 +16,25 @@ namespace api
 
 public class ServerInput : IDeviceInput
 {
-	string _serverURL;
+	protected string _serverURL;
+	protected HttpClient _client;
 	public ServerInput(string server_URL)
 	{
+		_client = new HttpClient();
+		_client.BaseAddress = new Uri(server_URL);
 		_serverURL = server_URL;
 	}
 
 	public bool read(Device dev)
 	{
+		if(dev == null)
+		{
+			return false;
+		}
 		var result = false;
 		try
 		{
-			var response = GetResponse(dev);
+			HttpResponseMessage response = GetResponse(dev);
 			if(response == null)
 			{
 				return result;
@@ -37,7 +44,7 @@ public class ServerInput : IDeviceInput
 			{
 				var content = response.Content.ReadAsStringAsync();
 				content.Wait();
-				result = Interfaces.UpdateDevice(dev, content.Result);
+				result = Interfaces.UpdateDevice(dev, content.Result, silence_io:false, update_id:false);
 			}
 		}
 		catch (Exception e)
@@ -53,16 +60,15 @@ public class ServerInput : IDeviceInput
 		HttpResponseMessage resp = null;
 		try
 		{
-			var client = new HttpClient();
-			client.Timeout = TimeSpan.FromSeconds(10);
-			client.BaseAddress = new Uri(_serverURL);
-			var resource_address = String.Format("/api/storage/device/{0}/{1}/{2}", dev1.ID.HouseID, dev1.ID.RoomID,
+			var resource_address = String.Format("api/storage/device/{0}/{1}/{2}", dev1.ID.HouseID, dev1.ID.RoomID,
 				dev1.ID.DeviceID);
-			resp = client.GetAsync(resource_address).Result;
+			var rr = _client.GetAsync(resource_address);
+			rr.Wait();
+			resp = rr.Result;
 		}
 		catch (Exception e)
 		{
-			Debug.WriteLine("Server Input Error: " + e.Message);
+			Debug.WriteLine("GetResponse Error: " + e.Message);
 		}
 
 		return resp;
